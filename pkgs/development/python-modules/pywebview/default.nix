57a2f44846b25fd6be8560cd898d80e7dc8366a1
ryneeverett <ryneeverett@gmail.com>
2021-02-25 14:33:50 -0500

    pywebview: 3.3.1 -> 3.4

--- pkgs/development/python-modules/pywebview/default.nix
+++ pkgs/development/python-modules/pywebview/default.nix
@@ -1,25 +1,33 @@
 { lib, buildPythonPackage, fetchFromGitHub, pythonOlder
-, importlib-resources, pytest, xvfb_run }:
+, importlib-resources, pytest, xvfb_run, qt5, pyqt5, pyqtwebengine }:
 
 buildPythonPackage rec {
   pname = "pywebview";
-  version = "3.3.1";
+  version = "3.4";
   disabled = pythonOlder "3.5";
 
   src = fetchFromGitHub {
     owner = "r0x0r";
     repo = "pywebview";
     rev = version;
-    sha256 = "015z7n0hdgkzn0p7aw1xsv6lwc260p8q67jx0zyd1zghnwyj8k79";
+    sha256 = "1vqcfdy61na9x16n4n8zv4c9aq7cdp3z292y28jyli981yvz14fw";
   };
 
-  propagatedBuildInputs = lib.optionals (pythonOlder "3.7") [ importlib-resources ];
+  nativeBuildInputs = [ qt5.wrapQtAppsHook ];
+
+  propagatedBuildInputs = [
+    pyqt5
+    pyqtwebengine
+  ] ++ lib.optionals (pythonOlder "3.7") [ importlib-resources ];
 
   checkInputs = [ pytest xvfb_run ];
 
   checkPhase = ''
+    export HOME=$(mktemp -d)
+    export QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
     pushd tests
     patchShebangs run.sh
+    substituteInPlace run.sh --replace PYTHONPATH=.. PYTHONPATH="$PYTHONPATH:.."
     xvfb-run -s '-screen 0 800x600x24' ./run.sh
     popd
   '';
